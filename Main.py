17f2287a758aad53dc1735b0c272b9ba7a2299e8
unknown <sparkyowns1@gmail.com>
2021-10-18 23:12:34 -0500

    credits for Mr. Riley

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import os
 import random
 
 def login(): #initial login sequence
-    with open('accounts.txt', 'r') as File:
+    with open('accounts.txt', 'w+') as File:
         find_account = File.read().find(',')
 
     if find_account == -1:
@@ -18,11 +18,11 @@ def login(): #initial login sequence
             print("Invalid Option \n")
             login()
 
-def account_login(file = 'accounts.txt'): #login IF they have an account
+def account_login(): #login IF they have an account
     username = input("Username: ")
     password = input("Password: ")
 
-    with open(file, 'r') as File:
+    with open('accounts.txt', 'w+') as File:
         find_account = File.read().find(username + ',' + password + '\n')
 
     if find_account != -1:
@@ -43,15 +43,23 @@ def account_create():
 
     character_create(username)
 
-def character_create(username):
-    char_name = input("Welcome to character creation! \nEnter a name for your character. \n")
+def charcter_picker():
+    character_create()
+    #Add stuff <---------------------------------------------------------
 
-    with open(username + '/characters.txt', 'a') as File:
-        File.write(char_name + '\n')
+def character_create(username):
+    char_name = input("Welcome to character creation!\nEnter a name for your character.\n")
+    char_class = input("Pick a class: Warrior, Hunter\n")
+
+    if char_class != "Warrior" or "Hunter":
+        print("Invalid option!\n")
+        character_create()
+
+    os.mkdir(username + "/" + char_name)
 
 def waiting():
-    status = input("What do you wish to do? Wander, Quest or PVP \n")
-    if status == "Wander":
+    status = input("What do you wish to do? Wander, Signout or Change character [W/S/CC]\n")
+    if status == "W":
         chance = random.randrange(1, 3) #determines event
         if chance == 1:
             print("*A wizard approaches* hello" + char_name)
@@ -59,27 +67,33 @@ def waiting():
             print("A wild coyote appears in front of you!")
         waiting()
 
-    elif status == "Quest":
-        print("No available quests")
-        waiting()
+    elif status == "S":
+        print("Signing out..")
+        login()
 
-    elif status == "PVP":
-        print("Coming soon")
-        waiting()
+    elif status == "CC":
+        print(char_name + " lays down and falls asleep.")
+        character_create() #Remember to change to character picker
 
     else:
-        print("Invalid Option \n")
+        print("Invalid Option")
         waiting()
 
 weapons = {
-    "common_wood_hammer": {'name': 'Wood Hammer', 'dmg': '1',},
-    "common_copper_sword": {},
+    "common_wood_hammer": {'name': 'Wood Hammer', 'dmg': 1,},
+    "common_copper_sword": {'name': 'Copper Sword', 'dmg': random.randrange(2,4)},
 }
+
+usables = {
+    "health_potion": {'name': 'Health Potion', 'hp': 10},
+}
+
+inventory = {}
 
 char_name = ""
 char_class = ""
-char_health = 0
-char_speed = 0
+char_hp = 100
+char_speed = 1
 char_weapon = "common_wood_hammer"
 char_dmg = weapons[char_weapon]['dmg']
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Wander by Gabriel "Light" Santiago
+# Thank you to Mr. Riley for teaching me python.
 A simple command line game made with python.
