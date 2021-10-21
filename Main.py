c2becedb870bbf70db19d83a1a1c573f361220a9
unknown <sparkyowns1@gmail.com>
2021-10-20 23:23:35 -0500

    fixed annoying character_create() bug

--- Main.py
+++ Main.py
@@ -30,7 +30,6 @@ def account_login(): #login IF they have an account
     else:
         print("Invalid User \n")
         login()
-    return username
 
 def account_create():
     username = input("Username: ")
@@ -43,28 +42,38 @@ def account_create():
 
     character_create(username)
 
-def charcter_picker():
-    character_create()
-    #Add stuff <---------------------------------------------------------
-
 def character_create(username):
     char_name = input("Welcome to character creation!\nEnter a name for your character.\n")
     char_class = input("Pick a class: Warrior, Hunter\n")
 
-    if char_class != "Warrior" or "Hunter":
+    if char_class in classes:
+        os.mkdir(username + "/" + char_name)
+    else:
         print("Invalid option!\n")
-        character_create()
+        character_create(username)
 
-    os.mkdir(username + "/" + char_name)
+classes = ["Warrior", "Hunter"]
+
+login() #DO NOT REMOVE
+
+weapons = {
+    "common_wood_hammer": {'name': "Wood Hammer", 'dmg': 1,},
+    "common_copper_sword": {'name': "Copper Sword", 'dmg': random.randint(2,4)},
+    "unique_god's-fury": {'name': "God's Fury", 'dmg': 100}
+}
+
+usables = {
+    "health_potion": {'name': 'Health Potion', 'hp': 10},
+}
 
 def waiting():
     status = input("What do you wish to do? Wander, Signout or Change character [W/S/CC]\n")
     if status == "W":
-        chance = random.randrange(1, 3) #determines event
+        chance = random.randint(2) #determines event
         if chance == 1:
-            print("*A wizard approaches* hello" + char_name)
+            print()
         elif chance == 2:
-            print("A wild coyote appears in front of you!")
+            print()
         waiting()
 
     elif status == "S":
@@ -72,30 +81,10 @@ def waiting():
         login()
 
     elif status == "CC":
-        print(char_name + " lays down and falls asleep.")
         character_create() #Remember to change to character picker
 
     else:
         print("Invalid Option")
         waiting()
 
-weapons = {
-    "common_wood_hammer": {'name': 'Wood Hammer', 'dmg': 1,},
-    "common_copper_sword": {'name': 'Copper Sword', 'dmg': random.randrange(2,4)},
-}
-
-usables = {
-    "health_potion": {'name': 'Health Potion', 'hp': 10},
-}
-
-inventory = {}
-
-char_name = ""
-char_class = ""
-char_hp = 100
-char_speed = 1
-char_weapon = "common_wood_hammer"
-char_dmg = weapons[char_weapon]['dmg']
-
-login()
 waiting()
